354da240ba239ca2416e40fb546effdab64b1790
Jhon YC <jhon_02_03@live.com>
2023-06-22 01:14:02 -0500

    update
    add progresbar
    add btn chat
    addicon

--- app/src/main/java/com/example/tramites/detalle.kt
+++ app/src/main/java/com/example/tramites/detalle.kt
@@ -1,14 +1,22 @@
 package com.example.tramites
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.content.res.ColorStateList
 import android.graphics.ColorMatrix
 import android.graphics.ColorMatrixColorFilter
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.example.tramites.databinding.ActivityDetalleBinding
 
 class detalle : AppCompatActivity() {
@@ -31,7 +39,13 @@ class detalle : AppCompatActivity() {
         imageColor(binding.iv5, 0f)
         imageColor(binding.iv6, 0f)
 
+        //color y % progressBar
+        extracted(Certificado.riesgo!!)
+
+
+
         if(Certificado.riesgo.equals("Bajo") || Certificado.riesgo.equals("Medio")){
+            binding.fabMain.isVisible = false
             //Comercializacion
             binding.view1.setBackgroundColor(ContextCompat.getColor(this, R.color.azulate));
 
@@ -42,6 +56,10 @@ class detalle : AppCompatActivity() {
                 }
                 "Con observaciones"->  {
                     imageColor(binding.iv2, 1f)
+                    binding.fabMain.isVisible = true
+                    //Agrear numero
+                    binding.fabMain.setOnClickListener { chatObs("+51912307990") }
+                    binding.tvDesaprobado.text = "Tiene 2 semana para subsanar"
                 }
                 "Finalizado"->  {
                     binding.view2.setBackgroundColor(ContextCompat.getColor(this, R.color.azulate));
@@ -64,7 +82,18 @@ class detalle : AppCompatActivity() {
                         "Desaprobado"->  {
                             imageColor(binding.iv5, 1f)
                             binding.iv5.setImageResource(R.drawable.cancelar)
+                            imageColor(binding.iv6, 1f)
                             binding.tvDesaprobado.text = "Intente denuevo el tramite"
+                        }
+                        "Con observaciones"->  {
+                            imageColor(binding.iv5, 1f)
+                            binding.iv5.setImageResource(R.drawable.observacion)
+                            binding.tvDatos.text = "Con Observaciones"
+                            binding.tvDesaprobado.text = "Tiene 2 semana para subsanar"
+                            binding.fabMain.isVisible = true
+                            //Agrear numero
+                            binding.fabMain.setOnClickListener { chatObs("+51912307990") }
+
                         }
                     }
                 }
@@ -73,6 +102,7 @@ class detalle : AppCompatActivity() {
 
 
         }else {
+            binding.fabMain.isVisible = false
             binding.view2.setBackgroundColor(ContextCompat.getColor(this, R.color.azulate));
             //Catastro
             when(Certificado.Estadoinspeccion){
@@ -98,6 +128,10 @@ class detalle : AppCompatActivity() {
                         }
                         "Con observaciones"->  {
                             imageColor(binding.iv2, 1f)
+                            binding.fabMain.isVisible = true
+                            //Agrear numero
+                            binding.fabMain.setOnClickListener { chatObs("+51912307990") }
+                            binding.tvDesaprobado.text = "Tiene 2 semana para subsanar"
                         }
                         "Finalizado"->  {
                             binding.view2.setBackgroundColor(ContextCompat.getColor(this, R.color.azulate));
@@ -118,10 +152,37 @@ class detalle : AppCompatActivity() {
                     binding.iv5.setImageResource(R.drawable.observacion)
                     binding.tvDatos.text = "Con Observaciones"
                     binding.tvDesaprobado.text = "Tiene 2 semana para subsanar"
+                    binding.fabMain.isVisible = true
+                    //Agrear numero
+                    binding.fabMain.setOnClickListener { chatObs("+51912307990") }
+
                 }
 
             }
 
+        }
+    }
+
+    private fun extracted(estadoR:String) {
+        binding.pbColores.progress = 100
+
+        when(estadoR){
+            "Bajo"-> {
+                binding.pbColores.setProgressCompat(25, true)
+                binding.pbColores.setIndicatorColor(ContextCompat.getColor(this,R.color.color1))
+            }
+            "Medio"-> {
+                binding.pbColores.setProgressCompat(50, true)
+                binding.pbColores.setIndicatorColor(ContextCompat.getColor(this,R.color.color2))
+            }
+            "Alto"-> {
+                binding.pbColores.setProgressCompat(75, true)
+                binding.pbColores.setIndicatorColor(ContextCompat.getColor(this,R.color.color3))
+            }
+            "Muy alto"-> {
+                binding.pbColores.setProgressCompat(100, true)
+                binding.pbColores.setIndicatorColor(ContextCompat.getColor(this,R.color.color4))
+            }
         }
     }
 
@@ -137,4 +198,21 @@ class detalle : AppCompatActivity() {
         val colorFilter = ColorMatrixColorFilter(colorMatrix)
         img.colorFilter = colorFilter
     }
+
+    private fun chatObs(toNumber:String){
+
+        val url = "https://api.whatsapp.com/send?phone=$toNumber"
+        try {
+            val pm: PackageManager = this.packageManager
+            pm.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES)
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            this.startActivity(i)
+        } catch (e: PackageManager.NameNotFoundException) {
+            this.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+        }
+    }
+
+
+
 }
